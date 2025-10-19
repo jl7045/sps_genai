4bbc78c9c93385ae9fb97e13ecce69e77acb40f5
jl7045 <jl7045@columbia.edu>
2025-10-19 19:55:14 -0400

    data_loader.py

--- helper_lib/data_loader.py
+++ helper_lib/data_loader.py
@@ -1,14 +1,5 @@
 import torch
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-
-def get_data_loader(data_dir="./data", batch_size=32, train=True):
-    tfm = transforms.Compose([
-        transforms.RandomHorizontalFlip() if train else transforms.Lambda(lambda x: x),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.2023, 0.1994, 0.2010))
-    ])
-    ds = datasets.CIFAR10(root=data_dir, train=train, download=True, transform=tfm)
-    loader = DataLoader(ds, batch_size=batch_size, shuffle=train, num_workers=2)
-    return loader
+def get_data_loader(data_dir, batch_size=32, train=True):
+# TODO: create the data loader
+return loader
