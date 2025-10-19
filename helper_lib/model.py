d2eda9acfa88a49e5d8f05ff7e81c08e826e8613
jl7045 <jl7045@columbia.edu>
2025-10-19 19:57:04 -0400

    model.py

--- helper_lib/model.py
+++ helper_lib/model.py
@@ -1,22 +1,6 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-class CNNModel(nn.Module):
-    def __init__(self, num_classes=10):
-        super(CNNModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.dropout = nn.Dropout(0.25)
-        self.fc1 = nn.Linear(64 * 8 * 8, 128)
-        self.fc2 = nn.Linear(128, num_classes)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 64 * 8 * 8)
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
-        return x
+from torchvision.models import resnet18
+def get_model(model_name):
+# TODO: define and return the appropriate model_name - one of: FCNN, CNN,
+EnhancedCNN
+return mode
