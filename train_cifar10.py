642759c29767883febab2dbfdbe5de6e0c7ebeb3
jl7045 <jl7045@columbia.edu>
2025-10-19 20:55:28 -0400

    train_cifar10.py

--- train_cifar10.py
+++ train_cifar10.py
@@ -1,3 +1,4 @@
+# train_cifar10.py
 from helper_lib.data_loader import get_loader
 from helper_lib.model import CNNModel
 from helper_lib.trainer import fit
@@ -6,14 +7,7 @@ def main():
     train_loader = get_loader("./data", batch_size=128, train=True)
     test_loader  = get_loader("./data", batch_size=256, train=False)
     model = CNNModel(num_classes=10)
-    fit(
-        model,
-        train_loader,
-        test_loader,
-        epochs=5,
-        lr=1e-3,
-        out_path="./models/cifar10_cnn.pt"
-    )
+    fit(model, train_loader, test_loader, epochs=5, lr=1e-3, out_path="./models/cifar10_cnn.pt")
 
 if __name__ == "__main__":
     main()
