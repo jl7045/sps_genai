4af5aa4aadcc40adc98e7abdd5ab2fade2903d9b
jl7045 <jl7045@columbia.edu>
2025-10-19 20:24:31 -0400

    data loader with 64x64 transforms

--- helper_lib/data_loader.py
+++ helper_lib/data_loader.py
@@ -1,18 +1,21 @@
-import torch
-from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 _MEAN = (0.4914, 0.4822, 0.4465)
-_STD = (0.2023, 0.1994, 0.2010)
+_STD  = (0.2023, 0.1994, 0.2010)
 
-def get_data_loader(data_dir="./data", batch_size=32, train=True):
-    tfm = transforms.Compose([
+def build_transforms(train: bool):
+    aug = [transforms.RandomHorizontalFlip()] if train else []
+    return transforms.Compose([
         transforms.Resize((64, 64)),
-        transforms.RandomHorizontalFlip() if train else transforms.Lambda(lambda x: x),
+        *aug,
         transforms.ToTensor(),
-        transforms.Normalize(_MEAN, _STD)
+        transforms.Normalize(_MEAN, _STD),
     ])
 
-    dataset = datasets.CIFAR10(root=data_dir, train=train, download=True, transform=tfm)
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=train, num_workers=2)
-    return loader
+def get_loader(data_dir: str = "./data", batch_size: int = 128, train: bool = True, num_workers: int = 2):
+    ds = datasets.CIFAR10(root=data_dir, train=train, download=True, transform=build_transforms(train))
+    return DataLoader(ds, batch_size=batch_size, shuffle=train, num_workers=num_workers)
+
+def class_names():
+    return ["airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"]
