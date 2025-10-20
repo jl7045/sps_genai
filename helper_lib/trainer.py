2d5f8b992d24d7de3df834104ac167a8413f4fbf
jl7045 <jl7045@columbia.edu>
2025-10-19 20:01:04 -0400

    trainer.py

--- helper_lib/trainer.py
+++ helper_lib/trainer.py
@@ -1,6 +1,4 @@
 import torch
-def train_model(model, data_loader, criterion, optimizer, device='cpu',
-epochs=10):
-# TODO: run several iterations of the training loop (based on epochs
-parameter) and return the model
+def train_model(model, data_loader, criterion, optimizer, device='cpu',epochs=10):
+# TODO: run several iterations of the training loop (based on epochsparameter) and return the model
 return model
