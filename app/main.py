8094d2a8aa1e57a66fc75bcf3f113bce7df86b63
jl7045 <jl7045@columbia.edu>
2025-10-19 21:47:19 -0400

    main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .infer import router as infer_router
 
-app = FastAPI(title="SPS GenAI – Image Classifier")
+app = FastAPI(title="SPS GenAI - Image Classifier")
 
 @app.get("/", tags=["health"])
 def root():
