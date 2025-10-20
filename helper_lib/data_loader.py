e525c84071bcc8dbcfd5bb6066b353342a460c80
jl7045 <jl7045@columbia.edu>
2025-10-19 21:57:01 -0400

    data_loader.py

--- helper_lib/data_loader.py
+++ helper_lib/data_loader.py
@@ -1,22 +1,35 @@
-# helper_lib/data_loader.py
-from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
-_MEAN = (0.4914, 0.4822, 0.4465)
-_STD  = (0.2023, 0.1994, 0.2010)
+def get_transforms(train: bool = True):
+    if train:
+        tfm = transforms.Compose([
+            transforms.Resize((64, 64)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+    else:
+        tfm = transforms.Compose([
+            transforms.Resize((64, 64)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+    return tfm
 
-def build_transforms(train: bool):
-    aug = [transforms.RandomHorizontalFlip()] if train else []
-    return transforms.Compose([
-        transforms.Resize((64, 64)),
-        *aug,
-        transforms.ToTensor(),
-        transforms.Normalize(_MEAN, _STD),
-    ])
-
-def get_loader(data_dir: str = "./data", batch_size: int = 128, train: bool = True, num_workers: int = 2):
-    ds = datasets.CIFAR10(root=data_dir, train=train, download=True, transform=build_transforms(train))
+def get_loader(data_dir: str = "./data", batch_size: int = 64, train: bool = True, num_workers: int = 2):
+    ds = datasets.CIFAR10(
+        root=data_dir,
+        train=train,
+        download=True,
+        transform=get_transforms(train)
+    )
     return DataLoader(ds, batch_size=batch_size, shuffle=train, num_workers=num_workers)
 
-def class_names():
-    return ["airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck"]
+def cifar10_class_names():
+    return [
+        "airplane","automobile","bird","cat","deer",
+        "dog","frog","horse","ship","truck"
+    ]
