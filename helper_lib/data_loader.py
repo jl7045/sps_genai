a959e974e014eb6d142ecf8a846b631da1e86a32
jl7045 <jl7045@columbia.edu>
2025-10-19 20:11:39 -0400

    data_loader.py

--- helper_lib/data_loader.py
+++ helper_lib/data_loader.py
@@ -1,5 +1,18 @@
 import torch
 from torchvision import datasets, transforms
-def get_data_loader(data_dir, batch_size=32, train=True):
-# TODO: create the data loader
-return loader
+from torch.utils.data import DataLoader
+
+_MEAN = (0.4914, 0.4822, 0.4465)
+_STD = (0.2023, 0.1994, 0.2010)
+
+def get_data_loader(data_dir="./data", batch_size=32, train=True):
+    tfm = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.RandomHorizontalFlip() if train else transforms.Lambda(lambda x: x),
+        transforms.ToTensor(),
+        transforms.Normalize(_MEAN, _STD)
+    ])
+
+    dataset = datasets.CIFAR10(root=data_dir, train=train, download=True, transform=tfm)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=train, num_workers=2)
+    return loader
