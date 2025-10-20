83d82dbbcf66df8e0ee46c78f5b08f8c48f0b4c4
jl7045 <jl7045@columbia.edu>
2025-10-19 20:03:28 -0400

    generator.py

---
+++ helper_lib/generator.py
@@ -0,0 +1,4 @@
+import torch
+def generate_samples(model, device, num_samples=10):
+# TODO: generate num_samples points in the latent space, run the decoder to construct an image, and plot the samples on a grid
+plt.show()
