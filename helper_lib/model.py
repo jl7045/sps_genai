868bf0349194977321f7e95043cf292831bd78da
jl7045 <jl7045@columbia.edu>
2025-10-19 20:01:32 -0400

    model.py

--- helper_lib/model.py
+++ helper_lib/model.py
@@ -1,6 +1,5 @@
 import torch.nn as nn
 from torchvision.models import resnet18
 def get_model(model_name):
-# TODO: define and return the appropriate model_name - one of: FCNN, CNN,
-EnhancedCNN
+#TODO: define and return the appropriate model_name - one of: FCNN, CNN,EnhancedCNN
 return mode
