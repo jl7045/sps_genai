585a6b89977653861e7a8cf7b7039891632e1d33
jl7045 <jl7045@columbia.edu>
2025-11-01 03:23:47 +0800

    Update gan_models.py

--- app/gan_models.py
+++ app/gan_models.py
@@ -1,1 +1,64 @@
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self, noise_dim=100):
+        super().__init__()
+        self.fc = nn.Linear(noise_dim, 128 * 7 * 7)
+        self.net = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=128,
+                out_channels=64,
+                kernel_size=4,
+                stride=2,
+                padding=1
+            ),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(
+                in_channels=64,
+                out_channels=1,
+                kernel_size=4,
+                stride=2,
+                padding=1
+            ),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        x = self.fc(z)
+        x = x.view(-1, 128, 7, 7)
+        img = self.net(x)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=64,
+                kernel_size=4,
+                stride=2,
+                padding=1
+            ),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=4,
+                stride=2,
+                padding=1
+            ),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Flatten(),
+            nn.Linear(128 * 7 * 7, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
