f557f0384e1684cdeabaeda809d75fc23b878a97
jl7045 <jl7045@columbia.edu>
2025-11-23 17:50:28 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 from .infer import router as infer_router
 from .gan_infer import router as gan_router
+from .energy_infer import router as energy_router
+from .diffusion_infer import router as diffusion_router
 
 app = FastAPI(title="SPS GenAI API")
+
 
 @app.get("/", tags=["health"])
 def root():
     return {"message": "OK"}
 
+
 app.include_router(infer_router)
 app.include_router(gan_router)
-<<<<<<< HEAD
-=======
-
->>>>>>> 5eba60f6627c0b39a9a28073b2ea2c69feffe620
+app.include_router(energy_router)
+app.include_router(diffusion_router)
